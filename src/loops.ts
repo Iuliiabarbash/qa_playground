39335103885bb62a5adf1b841dd2a0db1387be28
Yulia <iuliiabarbash@gmail.com>
2021-07-14 19:33:48 -0400

    loops added

---
+++ src/loops.ts
@@ -0,0 +1,28 @@
+// Ex1
+// for (let i = 0; i > -1; i++) {
+//     console.log(i)
+//     console.log(`Loop step ${i}`);
+// } 
+
+//Ex2
+// for (var cheesburger = 10; cheesburger > 0; cheesburger--) {
+//     console.log(`That is correct number of cheesburgers ${cheesburger}`);
+// }
+
+// //Ex3
+ let pieNames: Array<string> = ["Apple", "Peach", "Cherry"];
+
+ for(let i = 0; i <pieNames.length; i++) {
+     console.log(`I love ${pieNames[i]} Pie`);  
+ }
+
+//4
+
+// console.log("pie list:", pieNames);
+// for (let i =0; i <pieNames.length; i++) {
+//     pieNames[i] = pieNames[i].toLowerCase();
+//     console.log(`I love ${pieNames[i]} pie!`);
+// }
+// console.log('Pie list:', pieNames);
+    
+
